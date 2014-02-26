8ac2bc5c1dd14a04fc72bf381e9d2f0a1a3d9c95
Philip Persson <kalllez123@hotmail.se>
2014-02-26 15:09:10 +0100

    Added SpawnPoint Script

---
+++ Unity Project/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+
+	public string SpawnName = "defaultSpawnName";
+
+	public Vector3 GetSpawnPosition()
+	{
+		Vector3 v = transform.position;
+		v.y += 1;
+		return v;
+	}
+}
