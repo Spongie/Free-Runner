8ff007fc5aaaab34927ddbea95c2b7437a89b6b9
Philip Persson <kalllez123@hotmail.se>
2014-02-28 10:23:50 +0100

    SpawnPoint Testing done

---
+++ Unity Project/Assets/Scripts/SpawnFinder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFinder : MonoBehaviour {
+
+	public Transform player;
+	SpawnPoint[] spawnPoints;
+
+	void Start()
+	{
+		player.position = GetStartSpawn();
+		Debug.Log ("New Position: " + player.position);
+	}
+
+	public Vector3 GetStartSpawn()
+	{
+		GameObject[] objs = GameObject.FindGameObjectsWithTag ("SpawnPoint");
+		spawnPoints = new SpawnPoint[objs.Length];
+		for (int i = 0; i < objs.Length; i++)
+		{
+			spawnPoints[i] = objs[i].GetComponent<SpawnPoint>();
+			if(spawnPoints[i].SpawnName == "StartSpawn")
+				return spawnPoints[i].GetSpawnPosition();
+		}
+		return Vector3.zero;
+	}
+}
