2e81fff34ae135df1457d3f9b0ad139db7e7b270
Baginbix <maus_tim@hotmail.com>
2014-03-17 00:29:31 +0100

    AlertBar

---
+++ Unity Project/Assets/Scripts/Alarm.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alarm : MonoBehaviour {
+	public float maxAwareness;
+	public float awareness;
+	public float percentOfAwareness;
+	public float detectRadius;
+	public float awarenessRate;
+	public GUITexture alertBar;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKey)
+		{
+			RaiseAwareness();
+			ChangeAlertBar();
+		}
+
+		if(IsDetected())
+			Debug.Log("Detected Bitch");
+	}
+
+	public bool IsAwareOfPlayer()
+	{
+		float enemyDistanceFromPlayer = 10;
+		if(enemyDistanceFromPlayer<= detectRadius)
+			return true;
+		else 
+			return false;
+	}
+
+	public void RaiseAwareness()
+	{
+		awareness += awarenessRate;
+	}
+
+	public bool IsDetected()
+	{
+		return (awareness >= maxAwareness) ? true : false;
+	}
+
+	public void ChangeAlertBar()
+	{
+		percentOfAwareness = awareness/maxAwareness;
+		Rect alerter = alertBar.pixelInset;
+		alerter.height = (200*percentOfAwareness);
+		alertBar.pixelInset = alerter;
+	}
+}
