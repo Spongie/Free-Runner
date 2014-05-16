3127bbe2b5f19ea51d4ef80a9a42fceb0dd57bad
Fabian Ahlström <fabiandevinci@yahoo.se>
2014-05-16 16:48:38 +0200

    How-to-play ser bättre ut...igen
    woo /FABIAN

--- Unity Project/Assets/Scripts/FlipPageInTutorial.cs
+++ Unity Project/Assets/Scripts/FlipPageInTutorial.cs
@@ -12,53 +12,53 @@ public enum VilkenSida
 public class FlipPageInTutorial : MonoBehaviour {
 	
 	public VilkenSida sidan;
-	GameObject textTitle1, textTitle2, textTitle3, textActualText1, textActualText2, textActualText3, textureScreenshot;
+	public GameObject textTitle1, textTitle2, textTitle3, textActualText1, textActualText2, textActualText3, textureScreenshot;
 	
 	// Use this for initialization
 	void Start () {
 		sidan = VilkenSida.sida1;
-		textTitle1 = GameObject.Find("Tuturial_GUITxt_title1");
-		textTitle2 = GameObject.Find("Tutorial_GUITxt_title2");
-		textTitle3 = GameObject.Find("Tutorial_GUITxt_title3");
-		textActualText1 = GameObject.Find("Tutorial_GUITxt_pg1txt");
-		textActualText2 = GameObject.Find("Tutorial_GUITxt_pg2txt");
-		textActualText3 = GameObject.Find("Tutorial_GUITxt_pg3txt");
+		textTitle1 = GameObject.FindGameObjectWithTag("titel1");
+		textTitle2 = GameObject.FindGameObjectWithTag("titel2");
+		textTitle3 = GameObject.FindGameObjectWithTag("titel3");
+		textActualText1 = GameObject.FindGameObjectWithTag("text1");
+		textActualText2 = GameObject.FindGameObjectWithTag("text2");
+		textActualText3 = GameObject.FindGameObjectWithTag("text3");
 		textureScreenshot = GameObject.Find("Tutorial_GUITexture_Page2");
 	}
 	
 	#region Enable/Disable
 	private void EnableSida1()
 	{
-		textTitle1.Enabled = true;
-		textActualText1.Enabled = true;
+		textTitle1.SetActive(true);
+		textActualText1.SetActive(true);
 	}
 	private void EnableSida2()
 	{
-		textTitle2.Enabled = true;
-		textActualText2.Enabled = true;
-		textureScreenshot.Enabled = true;
+		textTitle2.SetActive(true);
+		textActualText2.SetActive(true);
+		textureScreenshot.SetActive(true);
 	}
 	private void EnableSida3()
 	{
-		textTitle3.Enabled = true;
-		textActualText3.Enabled = true;
+		textTitle3.SetActive(true);
+		textActualText3.SetActive(true);
 	}
 	
 	private void DisableSida1()
 	{
-		textTitle1.Enabled = false;
-		textActualText1.Enabled = false;
+		textTitle1.SetActive(false);
+		textActualText1.SetActive(false);
 	}
 	private void DisableSida2()
 	{
-		textTitle2.Enabled = false;
-		textActualText2.Enabled = false;
-		textureScreenshot.Enabled = false;
+		textTitle2.SetActive(false);
+		textActualText2.SetActive(false);
+		textureScreenshot.SetActive(false);
 	}
 	private void DisableSida3()
 	{
-		textTitle3.Enabled = false;
-		textActualText3.Enabled = false;
+		textTitle3.SetActive(false);
+		textActualText3.SetActive(false);
 	}
 	#endregion Enable/Disable
 	
@@ -66,6 +66,7 @@ public class FlipPageInTutorial : MonoBehaviour {
 	void Update () {
 		switch (sidan){
 		case VilkenSida.sida1:
+			Debug.Log("sida 1");
 			EnableSida1();
 			DisableSida2();
 			DisableSida3();
@@ -81,5 +82,19 @@ public class FlipPageInTutorial : MonoBehaviour {
 			EnableSida3();
 			break;
 		}
+	}
+
+	void OnGUI()
+	{
+		Rect sida1 = new Rect(20,420,450,150);
+		Rect sida2 = new Rect(20,570,450,150);
+		Rect sida3 = new Rect(20,720,450,150);
+		//GUI.Box(Rect(0, 0, 100, 100), "Sidor:");
+		if (GUI.Button(sida1, "Sida 1")){
+			sidan = VilkenSida.sida1;}
+		if (GUI.Button(sida2, "Sida 2")){
+			sidan = VilkenSida.sida2;}
+		if (GUI.Button(sida3, "Sida 3")){
+			sidan = VilkenSida.sida3;}
 	}
 }
